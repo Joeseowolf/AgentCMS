f70b8bf7026ff8f3a918c25e20d1e0aad20d9d90
Joeseowolf <36111242+Joeseowolf@users.noreply.github.com>
2018-03-01 00:24:52 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<head runat="server">
+	<title><?php echo $pageTitle;?></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css" />
+</head>
+<body>
